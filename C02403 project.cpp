7f8c6d0195c9c6b80523b8054860a25c36960492
aidenjones29 <36414445+aidenjones29@users.noreply.github.com>
2019-03-08 14:41:29 +0000

    push from aidens branch
    adwwad

--- C02403 project.cpp
+++ C02403 project.cpp
@@ -1,20 +1,34 @@
 // C02403 project.cpp: A program using the TL-Engine
 
 #include <TL-Engine.h>	// TL-Engine include file and namespace
-#include "ModelCreation.h"
 using namespace tle;
+
+class Weapon
+{
+
+};
+
 
 void main()
 {
 	// Create a 3D engine (using TLX engine here) and open a window for it
-	I3DEngine* myEngine = New3DEngine( kTLX );
+	I3DEngine* myEngine = New3DEngine(kTLX);
 	myEngine->StartWindowed();
 
 	// Add default folder for meshes and other media
-	myEngine->AddMediaFolder( ".\\Media" );
+	myEngine->AddMediaFolder(".\\Media");
+	ICamera* myCam = myEngine->CreateCamera(kFPS, 0, 7, 0);
+	myCam->SetMovementSpeed(5.0f);
 
 	/**** Set up your scene here ****/
+	IMesh* skyboxMesh = myEngine->LoadMesh("Skybox.x");
+	IMesh* floorMesh = myEngine->LoadMesh("Floor.x");
+	IMesh* FenceMesh = myEngine->LoadMesh("ChainLinkFence.x");
 
+	IModel* skybox = skyboxMesh->CreateModel(0.0f, -800.0f, 0.0f);
+	IModel* floor = floorMesh->CreateModel();
+	IModel* fence = FenceMesh->CreateModel();
+	fence->Scale(10);
 
 	// The main game loop, repeat until engine is stopped
 	while (myEngine->IsRunning())
@@ -24,6 +38,10 @@ void main()
 
 		/**** Update your scene each frame here ****/
 
+		if (myEngine->KeyHit(Key_Escape))
+		{
+			myEngine->Stop();
+		}
 	}
 
 	// Delete the 3D engine now we are finished with it
