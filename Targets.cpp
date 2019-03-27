a43782b5cf6b3eca0ff4f1e2c30c595490a0481b
DomSalmon <48459022+DomSalmon@users.noreply.github.com>
2019-03-27 06:22:29 +0000

    Added hostile targets
    Added targets (hostile)

--- Targets.cpp
+++ Targets.cpp
@@ -16,7 +16,57 @@ void spawnTargets(IMesh* targetMesh, vector<sTarget*> &vTargets)
 		target->state = Ready;
 		vTargets.push_back(target);
 	}
+	//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+	//COURSE TARGETS (HOSTILE)
+	const int numOfCourseTargets = 19;
+	IModel* courseTargets[numOfCourseTargets];
 
+	//Room 1
+	courseTargets[0] = targetMesh->CreateModel(80, 12, 160);
+	courseTargets[1] = targetMesh->CreateModel(80, 12, 200);
+	courseTargets[2] = targetMesh->CreateModel(60, 12, 240);
+	courseTargets[3] = targetMesh->CreateModel(50, 12, 220);
+	courseTargets[4] = targetMesh->CreateModel(50, 12, 140);
+
+	//Room 2
+	courseTargets[5] = targetMesh->CreateModel(260, 12, 290);
+	courseTargets[6] = targetMesh->CreateModel(260, 12, 340);
+
+	courseTargets[7] = targetMesh->CreateModel(260, 12, 140);
+	courseTargets[8] = targetMesh->CreateModel(260, 12, 180);
+
+	courseTargets[9] = targetMesh->CreateModel(190, 12, 260);
+
+	courseTargets[10] = targetMesh->CreateModel(190, 12, 180);
+	courseTargets[11] = targetMesh->CreateModel(210, 12, 180);
+
+	courseTargets[12] = targetMesh->CreateModel(310, 12, 100);
+	courseTargets[13] = targetMesh->CreateModel(325, 12, 140);
+	courseTargets[14] = targetMesh->CreateModel(340, 12, 100);
+
+	courseTargets[15] = targetMesh->CreateModel(290, 12, 100);
+	courseTargets[16] = targetMesh->CreateModel(250, 12, 100);
+	courseTargets[17] = targetMesh->CreateModel(185, 12, 95);
+	courseTargets[18] = targetMesh->CreateModel(190, 12, 15);
+
+	for (int i = 0; i < numOfCourseTargets; i++)
+	{
+		if (i == 0 || i == 1 || i == 2 || i == 7 || i == 8 || i == 9 || i == 17 || i == 18)
+		{
+			courseTargets[i]->RotateLocalY(-90);
+		}
+		if (i == 3 || i == 5 || i == 6)
+		{
+			courseTargets[i]->RotateLocalY(90);
+		}
+		if (i == 4 || i == 10 || i == 11 || i == 12 || i == 13 || i == 14)
+		{
+			courseTargets[i]->RotateLocalY(180);
+		}
+		courseTargets[i]->ScaleY(1.6);
+		courseTargets[i]->ScaleZ(0.1);
+	}
+	//-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 }
 void moveTargets(vector<sTarget*> &vTargets,float frameTime)
 {
