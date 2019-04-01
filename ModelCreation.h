f7a352c13333e744fab04a45ab97ca1e4425742b
aidenjones29 <36414445+aidenjones29@users.noreply.github.com>
2019-04-01 16:34:32 +0100

    awd
    awd

--- ModelCreation.h
+++ ModelCreation.h
@@ -1,7 +1,23 @@
 #pragma once
 #include <TL-Engine.h>
+#include <vector>
+#include <deque>
+
+const int FenceAmmount = 15;
+const int OuterWallAmount = 10;
+const int numOfgates = 3;
+const int numOfhallwayFences1 = 2;
 
 using namespace tle;
+
+struct fenceStruct
+{
+	IModel* fenceModel;
+	int fenceRotation;
+};
+
+deque<fenceStruct> allFences;
+
 
 void CreateScene(I3DEngine* myEngine);
 
